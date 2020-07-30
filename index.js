f373f46a6f271c52e366af0e9dcefd52fe7c28b8
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-30 21:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // Your code here
 class Polygon {
-
-  constructor(array) {
-    this.array = array;
-    this.count = this.array.length;
+  constructor(sides) {
+    this.sides = sides
+    this.count = this.sides.length
   }
 
-  get getCount () {
-    return this.array.length;
+  get getCount() {
+    return this.sides.length
   }
 
   get perimeter() {
-    let total = 0;
-    for (let i = 0; i < this.count; i++) {
-      total += this.array[i];
+    let sum = 0;
+    for (let i = 0; i < this.sides.length; i++) {
+      let side = this.sides[i];
+      sum += side;
     }
-    return total;
+    return sum
   }
 
 }
@@ -23,28 +23,36 @@ class Polygon {
 class Triangle extends Polygon {
 
   get isValid() {
-    // console.log(this.array)
-    let x = this.array[0];
-    let y = this.array[1];
-    let z = this.array[2];
-    return ((x + y > z) && (x + z > y) && (y + z > x)) ? true : false
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    if (this.count !== 3) {
+      return;
+    }
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side2 === side3 ) )
   }
 
 }
 
 class Square extends Polygon {
 
-  get isValid() {
-    // console.log(this.array)
-    let w = this.array[0];
-    let x = this.array[1];
-    let y = this.array[2];
-    let z = this.array[3];
-    return ((w === x) && (w === y) && (w === z)) ? true : false
+  get area() {
+    if (this.isValid) {
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      return side1 * side2
+    }
   }
 
-  get area() {
-    return this.array[0] ** 2;
+  get isValid() {
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+    if (this.count !== 4) {
+      return;
+    }
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side2 === side3 ) && ( side3 === side4 ) )
   }
 
 }
